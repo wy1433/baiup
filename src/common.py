c72fda5ca96f794158c976ea57c5adc7422aff21
yuzhong.chen <yuzhong.chen@ly.com>
2021-09-10 15:13:26 +0800

    cc

--- src/common.py
+++ src/common.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #-*- coding:utf8 -*-
 
-LOCAL_CACHE_DIR="./cache/clusters"
-LOCAL_SCRIPT_DIR="./script"
-REPO_DIR="./repo"
+LOCAL_CACHE_DIR="/home/work/tools/baiup/cache/clusters"
+LOCAL_SCRIPT_DIR="/home/work/tools/baiup/script"
+REPO_DIR="/home/work/tools/baiup/repo"
 BIN_NAME_DICT={"db":"baikaldb","meta":"baikalMeta","store":"baikalStore"}
-CLUSTER_DIR="./storage/clusters"
+CLUSTER_DIR="/home/work/tools/baiup/storage/clusters"
